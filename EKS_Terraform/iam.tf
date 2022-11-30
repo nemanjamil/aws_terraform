0839db755286e46cd03c79d0b9d719a9879c8527
Nemanja Milivojevic <nemanjamil@gmail.com>
2022-11-30 14:38:58 +0100

    Initial upload

---
+++ EKS_Terraform/iam.tf
@@ -0,0 +1,54 @@
+# Role policy
+resource "aws_iam_role" "runner" {
+  name = var.runner
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+# Attach policy to the role
+resource "aws_iam_role_policy_attachment" "runner-cluster-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.runner.name
+}
+
+# IAM for nodes
+resource "aws_iam_role" "nodes" {
+  name = "eks-node-group-nodes"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.nodes.name
+}
---
+++ EKS_Terraform/var.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  default = "eu-west-2"
+}
+
+variable "runner" {
+  default = "eks-runner-cluster"
+}
+variable "az-1a" {
+  default = "eu-west-2a"
+}
+variable "az-1b" {
+  default = "eu-west-2b"
+}
